235395b3dcde9e38629ee0631a58b71df8779d9b
giangndse183728 <giangndse183728@fpt.edu.vn>
2025-05-23 18:13:35 +0700

    giang/set up

---
+++ src/constants/seo.ts
@@ -0,0 +1,12 @@
+export const DEFAULT_SEO = {
+    title: "Digital Fashion | The Platform for Virtual Expression",
+    description: "Where virtual expression meets innovative design...",
+    keywords: ["digital fashion", "virtual runway", "meta fashion"],
+    image: {
+      url: "/fashion-visual-1.jpg",
+      width: 1200,
+      height: 630,
+      alt: "Digital Fashion Platform",
+    },
+  };
+  
